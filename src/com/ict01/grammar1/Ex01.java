632112b931dbba1da4d9fb1826f7637369210130
wlqja36 <wlqja36@naver.com>
2021-04-05 13:22:33 +0900

    test

--- src/com/ict01/grammar1/Ex01.java
+++ src/com/ict01/grammar1/Ex01.java
@@ -2,5 +2,9 @@ package com.ict01.grammar1;
 class Ex01{
 	public static void main(String[] args){
 		System.out.println("자바의 꿈!");
+		System.out.println("자바의 꿈!");
+		System.out.println("자바의 꿈!");
+		System.out.println("자바의 꿈!");
+		System.out.println("자바의 꿈!");
 	}
 }
