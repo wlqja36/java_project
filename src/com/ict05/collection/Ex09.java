80c95d91c8eabea26440cf63d276e6201bf78fe5
Beom <wlqja36@naver.com>
2021-04-06 18:24:15 +0900

    0406

--- src/com/ict05/collection/Ex09.java
+++ src/com/ict05/collection/Ex09.java
@@ -21,14 +21,14 @@ public class Ex09 {
 		map.put("태국", "방콕");
 		map.put("캐나다", "오타와");
 		// 나라 (key)를 set컬렉션에 넣음.
-		Set<String> keyset = map.keySet(); // 질문. map.containsKey ?
+		Set<String> keyset = map.keySet(); // 질문. map.containsKey --> 단일성, keyset으로 저장하면 다시 쓸 수있다.
 		Scanner sc = new Scanner(System.in);
 		esc:
 		while(true) {
 			System.out.print("나라 : ");
 			String con=sc.next();
 			
-			if(map.containsKey(con)) { //if(keyset.contains(con)) 
+			if(map.containsKey(con)) { //if(keyset.contains(con)) => 따로 저장해서 다시 쓸수있어서 사용
 				String res = map.get(con);
 				System.out.println(con+"의 수도는 "+res+"입니다.");
 				
